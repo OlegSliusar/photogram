0324dbc38d85a4591bb2fe989cf6e630a0fd8ba9
Oleh Sliusar <olehsliusar@gmail.com>
2018-05-02 14:07:03 +0300

    Set controller for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
-  
+
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
